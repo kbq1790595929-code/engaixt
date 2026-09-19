d42691fe0101e4b3b58c9bfdf4646b71aa9d0881
康保权 <3171482132@qq.com>
2026-09-20 03:40:38 +0800

    chore: keep public repository focused on application source

--- tools/lora_dataset/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Build local Japanese-to-Chinese LoRA datasets from translation caches."""
-
-from .models import CacheRow, CleanRecord, GameLabel
-
-__all__ = ["CacheRow", "CleanRecord", "GameLabel"]
